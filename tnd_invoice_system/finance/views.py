54c279ad15bed60c836cf8efd2cb26ff1188d946
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-18 11:37:30 +0000

    main

--- tnd_invoice_system/finance/views.py
+++ tnd_invoice_system/finance/views.py
@@ -3,9 +3,9 @@ from .models import District
 
 def district_list(request):
     districts = District.objects.all()
-    return render(request, "finance/district_list.html", {"districts": districts})
+    return render(request, "district_list.html", {"districts": districts})
 
 def district_detail(request, district_id):
     district = get_object_or_404(District, id=district_id)
     facilities = district.facilities.all()
-    return render(request, "finance/district_detail.html", {"district": district, "facilities": facilities})
+    return render(request, "district_detail.html", {"district": district, "facilities": facilities})
